310f2e69add985610446d490b5f29c8f0821c850
YoanMln <yoan.moulin@hotmail.fr>
2025-05-08 21:16:25 +0200

    MAJ diverse

--- github-activity.js
+++ github-activity.js
@@ -4,7 +4,8 @@ const url = `https://api.github.com/users/${username}/events`;
 fetch(url)
   .then((res) => {
     console.log(res);
-    res.json();
+
+    return res.json();
   })
   .then((events) => {
     const push = events.find((e) => e.type === "PushEvent");
@@ -13,12 +14,11 @@ fetch(url)
       const repo = push.repo.name;
       const msg = commit.message;
       document.getElementById("github-activity").innerHTML = `
-      <div class = "github-header">
-      <i class="fa-solid fa-code-fork"></i>
-      <h4>GitHub Activity</4>
-      
-      </div
-      <h5>Dernier commit sur le repo de <strong>${repo}</strong> :</h5>
+        <div class="github-header">
+          <i class="fa-solid fa-code-fork"></i>
+          <h4>GitHub Activity</h4>
+        </div>
+        <h5>Dernier commit sur le repo de <strong>${repo}</strong> :</h5>
         <p><em>"${msg}"</em></p>
       `;
     } else {
@@ -26,13 +26,12 @@ fetch(url)
         "Pas d'activité récente visible.";
     }
   })
-  .catch(() => {
+  .catch((error) => {
+    console.error("Erreur lors de la récupération des données:", error);
     document.getElementById("github-activity").innerHTML = `
-      <p> Erreur 403 API GitHub introuvable...🤡 <br> (Probablement une pause café)☕️
-      </p> 
-      
-      <p class="git-error" > En attendant, tu peux voir mon activité GitHub en cliquant ici : </p>
-        <a class="git-link"  href="https://github.com/YoanMln" target="_blank" class="icon-link">
-    <i class="fa-brands fa-github fa-2x btn-bounce" style="color: #37afe1"></i>
-  </a>`;
+      <p>API introuvable...<br> (Probablement une pause café)☕️</p>
+      <p class="git-error">En attendant, tu peux voir mon activité GitHub en cliquant ici :</p>
+      <a class="git-link" href="https://github.com/YoanMln" target="_blank" class="icon-link">
+        <i class="fa-brands fa-github fa-2x btn-bounce" style="color: #37afe1"></i>
+      </a>`;
   });
