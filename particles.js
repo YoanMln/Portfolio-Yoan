eac209af6d0862e85bd78dcd5a2b62b518455f51
YoanMln <yoan.moulin@hotmail.fr>
2025-05-10 18:06:16 +0200

    Améliorations particles-2

--- particles.js
+++ particles.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   particlesJS("particles-langages", {
     particles: {
-      number: { value: 30 },
+      number: { value: 45 },
       size: { value: 2 },
       color: { value: "#37afe1" },
     },
@@ -14,12 +14,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
   particlesJS("particles-projects", {
     particles: {
-      number: { value: 30 },
+      number: { value: 45 },
       size: { value: 2 },
       color: { value: "#37afe1" },
     },
     interactivity: {
-      events: { onhover: { enable: true, mode: "grab" } },
+      events: { onhover: { enable: true, mode: "repulse" } },
+    },
+    retina_detect: true,
+    fps_limit: 60,
+  });
+  particlesJS("particles-contact", {
+    particles: {
+      number: { value: 45 },
+      size: { value: 2 },
+      color: { value: "#37afe1" },
+    },
+    interactivity: {
+      events: { onhover: { enable: true, mode: "repulse" } },
     },
     retina_detect: true,
     fps_limit: 60,
