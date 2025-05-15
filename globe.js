ba6a36d9ec38c57ff70dbbb22035b8e61828784a
YoanMln <yoan.moulin@hotmail.fr>
2025-05-15 15:07:30 +0200

    Ajout Vanta.Globe

---
+++ globe.js
@@ -0,0 +1,14 @@
+VANTA.GLOBE({
+  el: "#accueil",
+  mouseControls: false,
+  touchControls: true,
+  gyroControls: true,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  size: 0.6,
+  color: 0x008dda,
+  color2: 0x2ddff3,
+  backgroundColor: 0x393e46,
+});
